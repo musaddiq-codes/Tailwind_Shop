b021d6edaed2d2e1f7a54608cd49b4448fa7e4ac
Musaddiq <85467906+musaddiq-codes@users.noreply.github.com>
2023-09-03 21:53:55 +0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const App = () => (
+  <main className='relative'>Nav
+  <section className='xl:padding-1 wide:padding-r padding-b'></section></main>
+)
+
+export default App
